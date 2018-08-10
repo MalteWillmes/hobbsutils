c8f00b11745e9ab15ddb7d7d9e4b9adc90ddde28
ctdenney <christian.denney@gmail.com>
2018-08-10 13:57:47 -0700

    sal_2_sr()
    Added sal_2_sr() function and documentation. Updated sr_2_sal documentaiton

--- hobbsutils/R/utils.R
+++ hobbsutils/R/utils.R
@@ -45,6 +45,18 @@ sr_2_sal = Vectorize(function(sr, srfw = 0.705264, srmar = 0.70918,confw = 74.6,
 
 })
 
+sal_2_sr = function(sal, srfw = 0.705264, srmar = 0.70918, confw = 74.6, conmar = 6819, salfw = 0.1, salmar = 31.8) {
+  if(sal < min(salfw, salmar) | sal > max(salfw, salmar)) {
+    warning('Your measured salinity is outside the bounds of your two endmembers, make sure that salfw and salmar are set correctly',
+            call. = F, immediate. = T)
+    return(NULL)
+  } else {
+    sr = ((((srfw*confw*sal)-(srfw*confw*salmar))/(salfw - salmar))+(srmar*conmar)-(((srmar*conmar*sal)-(srmar*conmar*salmar))/(salfw - salmar)))/
+      ((((confw*sal)-(confw*salmar))/(salfw - salmar))+(conmar)-(((conmar*sal)-(conmar*salmar))/(salfw-salmar)))
+    return(sr)
+  }
+}
+
 bim = function(fl, hl, gt) {
   fl + ((gt - max(gt))*(fl - hl))/(max(gt) - min(gt))
 }
