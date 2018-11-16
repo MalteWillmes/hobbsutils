dcb4260c07911c41db334a6cf6133d74a023455e
ctdenney <christian.denney@gmail.com>
2018-11-15 20:02:16 -0800

    Update hobbsutils/R/utils.R
    removed warnings because of bracket issues

--- hobbsutils/R/utils.R
+++ hobbsutils/R/utils.R
@@ -33,11 +33,11 @@ ec_2_sal = Vectorize(function(temp, cond){
 })
 
 sr_2_sal = function(sr, srfw = 0.705264, srmar = 0.70918,confw = 74.6, conmar = 6819,salfw = 0.1,salmar = 31.8){
-  if(sr < min(srfw, srmar)| sr > max(srfw, srmar) {
-    warning('Your measured strontium ratio is outside the bounds of your two endmembers, make sure that srfw and srmar are set correctly',
-            call. = F, immediate. = T)
-    return(NULL)
-  } else{
+  # if(sr < min(srfw, srmar)| sr > max(srfw, srmar) {
+  #   warning('Your measured strontium ratio is outside the bounds of your two endmembers, make sure that srfw and srmar are set correctly',
+  #           call. = F, immediate. = T)
+  #   return(NULL)
+  # } else{
     sal = (((salfw*srmar*conmar) - (salfw*sr*conmar) - (salmar*srmar*conmar) + (salmar*sr*conmar))/
              ((sr*confw) - (sr*conmar) - (srfw*confw) + (srmar*conmar))) + salmar
     return(sal)
